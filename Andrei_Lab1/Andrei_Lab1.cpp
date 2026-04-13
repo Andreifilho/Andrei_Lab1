531bf5ca1e4d2d2dcadc23ec994a6959ee96d0f2
Andrei Filho <contatoandreifilho@gmail.com>
2026-04-13 10:43:03 -0500

    add favourite number function'

--- Andrei_Lab1/Andrei_Lab1.cpp
+++ Andrei_Lab1/Andrei_Lab1.cpp
@@ -11,7 +11,6 @@ int main()
 	cout << "Git makes collaboration easy!" << endl;
 	cout << "Lab 1 completed on 4/8/262!" << endl;
 	cout << "Feature by Andrei Filho: Branching is awesome!" << endl;
-	cout << "TYPO LINE!!";).
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
